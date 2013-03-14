c816ec4c575472b2a39cfe22ea8af9a794c617cd
mukkoo <filippo.gangidino@welaika.com>
2013-03-14 13:22:09 +0100

    Plugin header

---
+++ users2csv.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Users to Csv
+ * @version 0.1
+ */
+/*
+Plugin Name: Users to Csv
+Plugin URI: http://github.com/welaika/users2Csv
+Description: Select and export Users data and metadata to a right formatted csv file.
+Version: 0.1
+Author: Mukkoo
+Author URI: http://github.com/mukkoo
+License: GPL2
+Text Domain: users-to-csv
+*/
