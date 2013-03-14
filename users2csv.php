4d7be59a953dc71b58730cc9d5fe3bba350b02f7
mukkoo <filippo.gangidino@welaika.com>
2013-03-14 14:09:14 +0100

    fields list

--- users2csv.php
+++ users2csv.php
@@ -29,6 +29,28 @@ class users_export {
     add_users_page( 'Users2Csv', 'Users2Csv', 'list_users', 'Users2Csv', array( $this, 'users_page' ) );
   }
 
+
+  /** Create a list of selectable fields **/
+  private function export_fields() {
+    global $wpdb;
+    // Default user fields
+    $data_keys_range = array('ID', 'user_login', 'user_pass', 'user_nicename', 'user_email', 'user_url', 'user_registered', 'user_activation_key', 'user_status', 'display_name');
+
+    // Collect usermeta fields
+    $meta_keys_range = $wpdb->get_results( "SELECT distinct(meta_key) FROM $wpdb->usermeta" );
+    $meta_keys_range = wp_list_pluck( $meta_keys_range, 'meta_key' );
+    $data_fields = "<strong>Default details:</strong><br />";
+    // Create checkboxes
+    foreach ($data_keys_range as $field) {
+      $data_fields .= '<input type="checkbox" name="field_default[]" value="'. $field .'">'. $field .'<br>';
+    }
+    $meta_fields = "<strong>Additional details:</strong><br />";
+    foreach ($meta_keys_range as $field) {
+      $meta_fields .= '<input type="checkbox" name="field_meta[]" value="'. $field .'">'. $field .'<br>';
+    }
+    $selectable_fields = $data_fields . $meta_fields;
+    return $selectable_fields;
+  }
 }
 
 new users_export;
